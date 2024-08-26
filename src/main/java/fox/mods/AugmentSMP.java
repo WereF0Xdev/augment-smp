cfe895f98f58c39df85e5b94e919248ac4fb5661
WereF0X <support@f0xmods.com>
2024-08-26 14:20:55 +0200

    Initial commit

---
+++ src/main/java/fox/mods/AugmentSMP.java
@@ -0,0 +1,17 @@
+package fox.mods;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AugmentSMP implements ModInitializer {
+	public static final String MOD_ID = "augment";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Augment SMP!");
+	}
+}
